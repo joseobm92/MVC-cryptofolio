fec3a2ccd3836e9dc4052a9e73e87f88790f8569
unknown <youssef.ojeil@hotmail.com>
2022-11-10 11:39:13 -0500

    Setup required models & create db

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,2 @@
-DROP DATABASE IF EXISTS crowdfund_db;
-CREATE DATABASE crowdfund_db;
+DROP DATABASE IF EXISTS cryptofolio_db;
+CREATE DATABASE cryptofolio_db;
---
+++ models/Cryptocurrency.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Cryptocurrency extends Model {}
+
+Cryptocurrency.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    rank: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    symbol: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'cryptocurrency',
+  }
+);
+
+module.exports = Cryptocurrency;
