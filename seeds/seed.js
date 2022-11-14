60f77b018d64986fb75bbf1d2e4f4cfb1628e3d2
bethdecarlo <bethdecarlo@gmail.com>
2022-11-14 12:40:23 -0500

    added code for all models to seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,9 +1,10 @@
 const sequelize = require('../config/connection');
-const { User,  } = require('../models');
+const { Cryptocurrency, Stat, User, Watchlist } = require('../models');
 
-const userData = require('./userData.json');
+
 const cryptocurrencyData = require('./cryptocurrencyData.json');
 const statData = require('./statData.json');
+const userData = require('./userData.json');
 const watchlistData = require('./watchlistData.json');
 
 const seedDatabase = async () => {
@@ -14,12 +15,28 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
+  const stats = await Stat.bulkCreate(statData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const cryptocurrencies = await Cryptocurrency.bulkCreate(cryptocurrencyData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const watchlists = await Watchlist.bulkCreate(watchlistData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+
+  // for (const project of projectData) {
+  //   await Project.create({
+  //     ...project,
+  //     user_id: users[Math.floor(Math.random() * users.length)].id,
+  //   });
+  // }
 
   process.exit(0);
 };
