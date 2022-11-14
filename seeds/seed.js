a9f4fc9f95f342ba10fa6330e63242480f61127c
bethdecarlo <bethdecarlo@gmail.com>
2022-11-14 12:28:28 -0500

    renamed seed json files

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,9 @@
 const sequelize = require('../config/connection');
 const { User, Project } = require('../models');
 
-const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const cryptocurrencyData = require('./Cryptocurrency.json');
+const statData = require('./Stat.json');
+const userData = require('./Userdata.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
