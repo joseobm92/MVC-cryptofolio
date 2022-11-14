f4582e74b8d6fe4c423e66bf9fbe973dc6851d6c
bethdecarlo <bethdecarlo@gmail.com>
2022-11-14 14:10:17 -0500

    added hooks to seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -10,25 +10,57 @@ const watchlistData = require('./watchlistData.json');
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const users = await User.bulkCreate(userData, {
+//seeds for cryptocurrency table from cryptocurrencyData.json
+const cryptocurrency = await Cryptocurrency.bulkCreate(cryptocurrencyData, {
+   
+  });
+
+// create seeds for stat table from statData.json
+const stat = await Stat.bulkCreate(statData, {
+   
+  });
+
+
+//create seeds for user table from userData.json
+//user had individual hooks
+
+const user = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
 
-  const stats = await Stat.bulkCreate(statData, {
-    individualHooks: true,
-    returning: true,
-  });
+//create seeds for watchlist table from watchlistData.json
+const watchlist = await Watchlist.bulkCreate(watchlistData, {
+   
+});
 
-  const cryptocurrencies = await Cryptocurrency.bulkCreate(cryptocurrencyData, {
-    individualHooks: true,
-    returning: true,
-  });
+//HOOKS
 
-  const watchlists = await Watchlist.bulkCreate(watchlistData, {
-    individualHooks: true,
-    returning: true,
-  });
+//for the cryptocurrency table, add a user_id row 
+for (const cryptocurrency of cryptocurrencyData) {
+    await Cryptocurrency.create({
+      ...cryptocurrency,
+      user_id: user[Math.floor(Math.random() * user.length)].id,
+    });
+  }
+
+  //for the stat table, add a cryptocurrceny_id row
+  for (const stat of statData) {
+    await Stat.create({
+      ...stat,
+      cryptocurrency_id: cryptocurrency[Math.floor(Math.random() * cryptocurrency.length)].id,
+    });
+  }
+
+
+//for the watchlist table, add a cryptocurrency_id row and a user_id row
+for (const watchlist of watchlistData) {
+    await Watchlist.create({
+      ...watchlist,
+      cryptocurrency_id: cryptocurrency[Math.floor(Math.random() * cryptocurrency.length)].id,
+      user_id: user[Math.floor(Math.random() * user.length)].id,
+    });
+  }
 
 
   // for (const project of projectData) {
