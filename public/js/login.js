4568bc0aa0054a7e6067311083249dcef25aa713
joseobm92 <joseobm92@gmail.com>
2022-11-22 09:59:25 -0500

    fixed minor typos

--- public/js/login.js
+++ public/js/login.js
@@ -20,7 +20,7 @@ const loginFormHandler = async (event) => {
 
     if (response.ok) {
       // If successful, redirect the browser to the profile page
-      document.location.replace('/watchlist');
+      document.location.replace('/');
     } else {
       alert(response.statusText);
     }
